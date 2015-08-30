30a474a004a8eda120122ff3f0d95d9d65ec5596
Ricardo Henrique Gracini Guiraldelli <ricardo.guiraldelli@gmail.com>
2015-08-30 12:20:18 +0200

    Remotion of loop and correction of search for the line of submission deadline
    Instead of a (wrong) for loop, now all is processed functionally in an
    array filter. Also correct the search for the line of submission
    deadline.

--- email_processor.js
+++ email_processor.js
@@ -12,25 +12,26 @@ function has_key_line(line){
 // verifies if the line contain the keywords for paper submission deadline date,
 // returning a true value in positive case
 function is_paper_deadline(line){
-    return line.test(REGEX_PAPER_DEADLINE);
+    return REGEX_PAPER_DEADLINE.test(line);
 }
 
 // takes a GmailMessage object and process it, extracting
 function process_email(gmail_message){
     var subject = get_subject_text(gmail_message);
-    var lines_body = break_lines(get_message_text(gmail_message));
-    for (line in lines_body){
-        if (has_date(line) && is_paper_deadline(line)){
-            var date = get_date(get_literal_date(line));
-            var calendar_event = create_event(subject, date);
-            if (calendar_event == null){
-                Logger.log("It was not possible to create an event with the following details:\n\tSubject: %s\n\tDate: %s", subject, date);
-                gmail_message.star();
-            }
-            else{
-                gmail_message.markRead();
-            }
-            break;
-        }
+    Logger.log("\tProcessing email '%s'", subject);
+    var lines_of_interest = break_lines(get_message_text(gmail_message)).filter(has_date).filter(is_paper_deadline);
+    // process only one entry of lines of interest
+    if (lines_of_interest.length > 0){
+        var date = get_date(get_literal_date(lines_of_interest[0]));
+        Logger.log("\t\tExtracted Date: %s", date);
+        // var calendar_event = create_event(subject, date);
+        // if (calendar_event == null){
+        //     Logger.log("It was not possible to create an event with the following details:\n\tSubject: %s\n\tDate: %s", subject, date);
+        //     gmail_message.star();
+        // }
+        // else{
+        //     Logger.log("\t\tMarking email as read.");
+        //     gmail_message.markRead();
+        // }
     }
 }
